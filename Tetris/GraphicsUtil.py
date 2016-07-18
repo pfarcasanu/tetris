191e7e6f22e4d7e4360a5821129714c0d7a37319
pfarcasanu <pfarcasanu@gmail.com>
2016-07-18 13:43:56 -0400

    File location changes

--- Tetris/GraphicsUtil.py
+++ Tetris/GraphicsUtil.py
@@ -12,6 +12,8 @@ winLen = 300
 winWidth = 510
 gridList = []
 grid = pygame.Surface((winLen, winWidth))
+grid.set_colorkey(BLACK)
+
 #toplength etc
 toplength = int(winLen/10) +1
 bottomlength = int(winLen *9/10) + 1
@@ -53,7 +55,7 @@ TGrid = [[0,0,0,0,0,0,0,0,0,0],
 # set the background 
 # ------------------------------
 bckgdSurface = pygame.Surface((500,500))
-bckgd = pygame.image.load("C:/Users/floro/Desktop/Python Programs/Tetris-Rose-Hulman-Project/GalaxyBkgd.jpg")
+bckgd = pygame.image.load("GalaxyBkgd.jpg")
 bckgd = pygame.transform.scale(bckgd, (500,500))
 
 bckgdSurface.blit(bckgd, (0,0))
@@ -61,7 +63,7 @@ bckgdSurface.blit(bckgd, (0,0))
 # ------------------------------
 scoreSurface = pygame.Surface((200, 50))
 
-scoreImage = pygame.image.load("C:/Users/floro/Desktop/Python Programs/Tetris-Rose-Hulman-Project/ScoreBox.png")
+scoreImage = pygame.image.load("ScoreBox.png")
 scoreImage = pygame.transform.scale(scoreImage, (200, 50))
 
 scoreSurface.blit(scoreImage, (0, 0))
