1b60e4929be136b0c3c051254679ee6689fff65b
pfarcasanu <pfarcasanu@gmail.com>
2016-07-18 13:53:50 -0400

    Sync up with George

--- Tetris/GraphicsUtil.py
+++ Tetris/GraphicsUtil.py
@@ -30,7 +30,7 @@ for i in range (toplength,bottomlength,width):
 for i in range(len(gridList)):
         a = gridList[i]
         rect = pygame.Rect(a, (width, width))
-        pygame.draw.rect(grid, (0,0,250), rect,2)
+        pygame.draw.rect(grid, (WHITE), rect,2)
 TGrid = [[0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -57,14 +57,16 @@ TGrid = [[0,0,0,0,0,0,0,0,0,0],
 bckgdSurface = pygame.Surface((500,500))
 bckgd = pygame.image.load("GalaxyBkgd.jpg")
 bckgd = pygame.transform.scale(bckgd, (500,500))
-
 bckgdSurface.blit(bckgd, (0,0))
 
+# Score Box
 # ------------------------------
 scoreSurface = pygame.Surface((200, 50))
-
 scoreImage = pygame.image.load("ScoreBox.png")
 scoreImage = pygame.transform.scale(scoreImage, (200, 50))
-
 scoreSurface.blit(scoreImage, (0, 0))
 
+#Tetraminos!
+#-------------------
+
+#Tetra
