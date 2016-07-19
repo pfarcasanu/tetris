ab047a53f4310f68b558dba7c027e5cf1123f214
sam-dunaway <sam.dunaway@yahoo.com>
2016-07-19 15:12:43 -0400

    comment problems

--- Tetris/Tblock.py
+++ Tetris/Tblock.py
@@ -33,19 +33,19 @@ class TBlock:
     
 
 
-while True:
-    if __name__ == "__main__":
-        pygame.init()
-        screen = pygame.display.set_mode((500, 500))
-        b = TBlock(200,200)
-        screen.fill((255, 255,255))
-        b.draw(screen)
+# while True:
+#     if __name__ == "__main__":
+#         pygame.init()
+#         screen = pygame.display.set_mode((500, 500))
+#         b = TBlock(200,200)
+#         screen.fill((255, 255,255))
+#         b.draw(screen)
 
-        # screen.blit(b, (200, 200))
-        eventList = pygame.event.get()
-        for event in eventList:
-            if event.type == pygame.QUIT:
-                    # if someone tries to close the Windows
-                    exit()
+#         # screen.blit(b, (200, 200))
+#         eventList = pygame.event.get()
+#         for event in eventList:
+#             if event.type == pygame.QUIT:
+#                     # if someone tries to close the Windows
+#                     exit()
 
-        pygame.display.flip()        
+#         pygame.display.flip()        
